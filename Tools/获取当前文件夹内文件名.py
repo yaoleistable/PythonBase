fabca56f224ca6b15b76353beb6a721da4bb432e
yaolei <815141681@qq.com>
2019-10-11 22:19:53 +0800

    文件操作实例

--- Tools/获取当前文件夹内文件名.py
+++ Tools/获取当前文件夹内文件名.py
@@ -9,25 +9,27 @@ import os
 
 
 def get_file_name(file_dir):
+    i = 1
+    j = 1
     for root, dirs, files in os.walk(file_dir):
         # print(root)
         # print(dirs)
         # print('文件：', files)
         for file in files:
-            print("文件名", file)
-        # 方法一：
-        # with open("文件名1.txt", "w") as f:
-        # f.writelines(str(files))
+            print(str(i) + ' ' + file)
+            i = i + 1
+            # 方法一：
+            # with open("文件名1.txt", "a") as f:
+            # f.writelines(str(files))
         # 方法二：
-        # with open("文件名2.txt", "w") as f:
+        # with open("当前文件夹内文件名.txt", "a") as f:
         # for file in files:
-        # f.writelines(file + '\n')
-        # f.write(file+'\n')
+        # file_name = str(j)+' '+file+'\n'
+        # f.writelines(file_name)
         # 方法三：
-        s = '\n'
-        with open("当前文件夹内文件名.txt", "w") as f:
-            f.writelines(s.join(files))
-            # f.write(s.join(files))
+            s = '\n'
+            with open("当前文件夹内文件名.txt", "a") as f:  # 注意需用a模式，用w模式，每次写会清空上一次写的内容
+                f.writelines(s.join(file))
     print("读取当前文件夹内文件名成功！")
 
 
